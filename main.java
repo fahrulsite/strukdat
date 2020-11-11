038b6d6ce11e619db3a097b73bef383db2480c96
fahrul <fahrul@mixRoBa.localdomain>
2020-11-11 14:24:45 +0700

    flowchart1

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ class main{
          boolean loop = true;
          while(loop){
              System.out.println("Pilihan menu \n1. EnQueue\n2. DeQueue\n3. Melihat data paling atas\n4. Menghapus semua data(clear)\n5. Melihat isi queue(tampil)\n6. Keluar");
+             System.out.print("Pilihan menu : ");
              int pilMenu = scan.nextInt();
              switch (pilMenu) {
                  case 1:
