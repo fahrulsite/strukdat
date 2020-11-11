29a157e55704806662f4f93b72739eff7da8bed8
fahrul <fahrul@mixRoBa.localdomain>
2020-11-11 14:16:15 +0700

    flowchart1

---
+++ main.java
@@ -0,0 +1,29 @@
+import java.text.BreakIterator;
+import java.util.*;
+class main{
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         boolean loop = true;
+         while(loop){
+             System.out.println("Pilihan menu \n1. EnQueue\n2. DeQueue\n3. Melihat data paling atas\n4. Menghapus semua data(clear)\n5. Melihat isi queue(tampil)\n6. Keluar");
+             int pilMenu = scan.nextInt();
+             switch (pilMenu) {
+                 case 1:
+                     break;
+                 case 2:
+                     break;
+                 case 3:
+                     break;
+                 case 4:
+                    break;
+                 case 5:
+                    break;
+                 case 6:
+                    System.exit(0);
+                    break;
+                 default:
+                     break;
+             }
+         }
+     }
+ }
